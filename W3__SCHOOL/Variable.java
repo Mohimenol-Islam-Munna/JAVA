ff21df2549f6651037aa503476a51c343b2538e4
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-12-20 12:50:28 +0600

    variables

--- W3__SCHOOL/Variable.java
+++ W3__SCHOOL/Variable.java
@@ -12,7 +12,18 @@ public class Variable {
         String name = "munna";
         int id = 160133;
 
-        System.out.println(name);
-        System.out.println(id);
+        System.out.println("Name : " + name);
+        System.out.println("Id :" + id);
+
+        String sub;
+        sub = "CSE";
+        System.out.println("Subject : " + sub);
+
+        int a, b, c;
+        a = 10;
+        b = c = 20;
+        System.out.println("Multiple Variable : " + a + b + c);
+        System.out.println(a + b + c);
+        System.out.println(a + b + c + " : Multiple Variable");
     }
 }
