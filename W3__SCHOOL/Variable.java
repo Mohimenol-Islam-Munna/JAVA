fbf1c1d3b5159e484b25c7cb3034873b80addd84
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-12-20 12:40:51 +0600

    variable

---
+++ W3__SCHOOL/Variable.java
@@ -0,0 +1,18 @@
+/* 
+
+variable syntax:
+    dataType variableName = value
+
+*/
+
+public class Variable {
+    public static void main(String[] args) {
+        System.out.println("variables in java");
+
+        String name = "munna";
+        int id = 160133;
+
+        System.out.println(name);
+        System.out.println(id);
+    }
+}
