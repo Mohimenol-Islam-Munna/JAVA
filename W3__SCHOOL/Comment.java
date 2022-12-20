51dcc8c184489f7a7b64f162031fea1b9103816d
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-12-20 12:32:13 +0600

    comments in java

---
+++ W3__SCHOOL/Comment.java
@@ -0,0 +1,8 @@
+public class Comment {
+    public static void main(String[] args){
+        // single line comment 
+        /* multi line comment 
+        in java */
+        System.out.println("comments in java");
+    }
+}
