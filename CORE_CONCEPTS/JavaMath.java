897426c9dc63faf9232f3881470ed203e6bb06d3
unknown <munna.cse.pust@gmail.com>
2022-12-26 22:29:40 +0600

    ok

---
+++ CORE_CONCEPTS/JavaMath.java
@@ -0,0 +1,10 @@
+public class JavaMath{
+  public static void main(String[] args){
+    System.out.println("Java Math Class");
+
+    int result = Math.max(100, 151);
+
+    // max number 
+    System.out.println(result);
+  }
+}
